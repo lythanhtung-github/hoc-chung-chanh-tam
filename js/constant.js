4436e58f5b46f7ad032fb3c9dcadf79aa5f64c84
lythanhtung-github <lythanhtung.work@gmail.com>
2025-12-25 21:00:45 +0700

    fix show default image

--- js/constant.js
+++ js/constant.js
@@ -15,6 +15,7 @@ const Constant = (() => {
         NAME_ASC: "name-asc",
         NAME_DESC: "name-desc",
         ITEMS_PER_PAGE: 8,
+        DEFAULT_AVATAR: "./images/USER.png",
         TAB_LIMITS: TAB_LIMITS,
         MESSAGE: MESSAGE,
         DATA_EXCEL_URL: "./data/DATA.xlsx",
